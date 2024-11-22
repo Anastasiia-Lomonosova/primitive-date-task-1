b582bb3d8da9d6f5975f78f02775e523b8b897d8
AnastaL <anastasia_los@mail.ru>
2024-11-22 14:37:34 +0300

    initial commit

---
+++ primitive data and operation/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double ticket = 21700;
+        int rubForMile = 20;
+        int mile = (int) (ticket / rubForMile);
+
+        System.out.println(mile);
+    }
+}
